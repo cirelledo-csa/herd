c57a331f8d83d1f0c34090cfdfddc2c748c69b3d
Eric Odell <eric.odell@ucop.edu>
2021-04-26 10:11:20 -0700

    os versions of brewfile

--- bin/brew.sh
+++ bin/brew.sh
@@ -4,12 +4,23 @@ echo make sure 'eval $(~/.linuxbrew/bin/brew shellenv)'  is in your ~/.bashrc
 if ! command -v brew &> /dev/null
 then
 	echo "brew not found, downloading and installing"
+	mkdir -p ~/.linuxbrew/bin
 	git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
-	mkdir ~/.linuxbrew/bin
 	ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
 else
 	echo "brew found, nothing doing"
 fi
+
+
+if [[ "$OSTYPE" == "linux"* ]]
+then
+    brewfile="Brewfile-linux"
+elif [[ "$OSTYPE" == "darwin"* ]]
+then
+    brewfile="Brewfile-macosx"
+else
+  "can't find os, exiting";exit
+fi
 eval "$(~/.linuxbrew/bin/brew shellenv)"
-brew bundle --file=Brewfile
+brew bundle --file="$brewfile"
 brew link --overwrite  awscli
