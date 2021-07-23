5d4f78b3a7ed5346919d649db57eaedc21b8ec22
Eric Odell <cirelledo@gmail.com>
2021-07-23 11:27:03 -0700

    less worse sso profile generator

--- bin/sso-profiles.sh
+++ bin/sso-profiles.sh
@@ -3,14 +3,11 @@ echo "this script will attempt to create aws cli profiles from your sso profiles
 echo "you'll need acces to a working sso login credential"
 echo "hint you can do this with 'aws sso login --profile youreasytorememberprofile'"
 echo "here are the cache files aws uses for sso authentication"
-ls -ltar ~/.aws/sso/cache/*
+ls -ltar ~/.aws/sso/cache/* |grep -v botocore
 echo
 echo "please enter the fully qualified most recent aws sso cache file(not the botocore-client-id-us-west-2.json"
 read filename
 token=$(cat "$filename" | jq -r .accessToken)
-echo
-echo "enter an optional prefix for profiles, eg my-org- (include a terminating dash as profiles will be named $prefix$account_name-$role)"
-read prefix
 echo "The following output can be added to ~/.aws/config"
 echo
 echo "enter your sso portal name, eg the value of yourportal in https://yourportal.awsapps.com/start#/"
@@ -20,7 +17,7 @@ do
 	account_name=$(echo $line | awk -F , '{print $1}' | sed -e 's/ /-/g');
 	account_id=$(echo $line | awk -F , '{print $2}');
 	for role in $(aws sso list-account-roles  --account-id $account_id --access-token $token  | jq -r '.roleList[].roleName'); do
-		echo "[profile $prefix$account_name-$role]";
+		echo "[profile $portal-$account_name-$role]";
 		echo output = json;
 		echo region = us-west-2;
 		echo sso_account_id = $account_id;
